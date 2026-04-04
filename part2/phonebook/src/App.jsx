0ec2b5443f08e49b248d1a202e2a97909fa59904
pawansai-code <pawansairdj17@gmail.com>
2026-04-04 14:01:53 +0530

    implemented a new server with a DB in json formmat and made functional with crud operations in the phonebook

---
+++ part2/phonebook/src/App.jsx
@@ -0,0 +1,90 @@
+// src/App.jsx
+import { useState, useEffect } from 'react'
+import personService from './services/persons'
+import Filter from './components/Filter'
+import PersonForm from './components/PersonForm'
+import Persons from './components/Persons'
+
+const App = () => {
+  const [persons, setPersons] = useState([])
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [filter, setFilter] = useState('')
+
+  useEffect(() => {
+    personService
+      .getAll()
+      .then(response => {
+        setPersons(response.data)
+      })
+  }, [])
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    
+    const existingPerson = persons.find(p => p.name === newName)
+    const personObject = {
+      name: newName,
+      number: newNumber
+    }
+
+    if (existingPerson) {
+      if (window.confirm(`${newName} already exists, replace number?`)) {
+        personService
+          .update(existingPerson.id, personObject)
+          .then(response => {
+            setPersons(persons.map(p => p.id !== existingPerson.id ? p : response.data))
+            setNewName('')
+            setNewNumber('')
+          })
+      }
+    } else {
+      personService
+        .create(personObject)
+        .then(response => {
+          setPersons(persons.concat(response.data))
+          setNewName('')
+          setNewNumber('')
+        })
+    }
+  }
+
+  const deletePerson = id => {
+    if (window.confirm("Delete this person?")) {
+      personService
+        .remove(id)
+        .then(() => {
+          setPersons(persons.filter(p => p.id !== id))
+        })
+    }
+  }
+
+  const handleNameChange = (event) => setNewName(event.target.value)
+  const handleNumberChange = (event) => setNewNumber(event.target.value)
+  const handleFilterChange = (event) => setFilter(event.target.value)
+
+  const personsToShow = filter
+    ? persons.filter(p => p.name.toLowerCase().includes(filter.toLowerCase()))
+    : persons
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <Filter filter={filter} handleFilterChange={handleFilterChange} />
+
+      <h3>Add a new</h3>
+      <PersonForm
+        addPerson={addPerson}
+        newName={newName}
+        handleNameChange={handleNameChange}
+        newNumber={newNumber}
+        handleNumberChange={handleNumberChange}
+      />
+
+      <h3>Numbers</h3>
+      <Persons persons={personsToShow} deletePerson={deletePerson} />
+    </div>
+  )
+}
+
+export default App
